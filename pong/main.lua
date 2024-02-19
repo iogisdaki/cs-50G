8483018c439b73a64070817ec0461d09bfc24582
iogisdaki <iogisdaki@gmail.com>
2024-02-19 18:31:25 +0200

    pong-3 puddles moving

--- pong/main.lua
+++ pong/main.lua
@@ -8,10 +8,13 @@ WINDOW_HEIGHT = 1500
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest') -- nearest neighbour filtering
 
     smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     love.graphics.setFont(smallFont)
 
@@ -20,6 +23,29 @@ function love.load()
         resizable = false,
         vsync = true -- (vertical sync) synced to monitor refresh rate
     })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+-- runs every frame
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + (-PADDLE_SPEED) * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    --player 2 movement
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + (-PADDLE_SPEED) * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
 end
 
 function love.keypressed(key)
@@ -35,14 +61,20 @@ function love.draw()
     -- clear the screen with this colour
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
     
+    love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-    --left puddle
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-    -- right paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
-    -- ball
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2, 4, 4)
+    -- print scores
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
+    --left puddle
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    -- right paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    
     push:apply('end')
 end
