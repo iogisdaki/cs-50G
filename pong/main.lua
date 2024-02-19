d081299f6f5276a14bba4541bc6a18411389d9a3
iogisdaki <iogisdaki@gmail.com>
2024-02-19 17:32:07 +0200

    pong-2

--- pong/main.lua
+++ pong/main.lua
@@ -11,6 +11,10 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest') -- nearest neighbour filtering
 
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -28,13 +32,17 @@ function love.draw()
     -- begin rendering at virtual resolution
     push:apply('start')
 
-    love.graphics.printf(
-        'Hello Pong!', -- text to render
-        0, -- starting x
-        VIRTUAL_HEIGHT / 2 - 6, -- starting y (default font in 12 pixels)
-        VIRTUAL_WIDTH, -- align with the full window
-        'center' -- alignment mode
-    )
+    -- clear the screen with this colour
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    --left puddle
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    -- right paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2, 4, 4)
 
     push:apply('end')
 end
