fe2152a1d335b79427a4e295511c18a075e167c1
iogisdaki <iogisdaki@gmail.com>
2024-02-20 18:49:01 +0200

    pong-5 separated into classes

--- pong/main.lua
+++ pong/main.lua
@@ -1,6 +1,12 @@
--- requirring library push
+-- requiring library push
 -- push helps take a window and turn in into virtual resolution window
 push = require 'push'
+
+-- class helps to write object oriented code
+Class = require 'class'
+
+require 'Paddle'
+require "Ball"
 
 WINDOW_WIDTH = 1700
 WINDOW_HEIGHT = 1500
@@ -16,7 +22,6 @@ function love.load()
     math.randomseed(os.time())
 
     smallFont = love.graphics.newFont('font.ttf', 8)
-    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     love.graphics.setFont(smallFont)
 
@@ -26,17 +31,9 @@ function love.load()
         vsync = true -- (vertical sync) synced to monitor refresh rate
     })
 
-    player1Score = 0
-    player2Score = 0
-
-    player1Y = 30
-    player2Y = VIRTUAL_HEIGHT - 50
-
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-
-    ballDX = math.random(2) == 1 and 100 or -100 -- ternary if random == 1 set to 100 else set to -100
-    ballDY = math.random(-50, 50)
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     gameState = 'start'
 
@@ -46,24 +43,30 @@ end
 function love.update(dt)
     -- player 1 movement
     if love.keyboard.isDown('w') then
-        player1Y = math.max(0, player1Y + (-PADDLE_SPEED) * dt)
+        player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
 
     --player 2 movement
     if love.keyboard.isDown('up') then
-        player2Y = math.max(0, player2Y + (-PADDLE_SPEED) * dt)
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
     end
 
     -- update ball
     -- scale the velocity by dt so movement is framerate-independent
     if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
+        ball:update(dt)
     end
+
+    player1:update(dt)
+    player2:update(dt)
 end
 
 function love.keypressed(key)
@@ -74,16 +77,7 @@ function love.keypressed(key)
             gameState = 'play'
         else
             gameState = 'start'
-            
-            -- start ball's position in the middle of the screen
-            ballX = VIRTUAL_WIDTH / 2 - 2
-            ballY = VIRTUAL_HEIGHT / 2 - 2
-
-            -- given ball's x and y velocity a random starting value
-            -- the and/or pattern here is Lua's way of accomplishing a ternary operation
-            -- in other programming languages like C
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
+            ball:reset()
         end
     end
 end
@@ -102,16 +96,13 @@ function love.draw()
         love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
     end
     -- print scores
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    -- love.graphics.setFont(scoreFont)
+    -- love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    -- love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
-    --left puddle
-    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
-    -- right paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-    -- ball
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+    player1:render()
+    player2:render()
+    ball:render()
     
     push:apply('end')
 end
