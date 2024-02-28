b5379394424188905e8bf239820e5ef4c7f387ac
iogisdaki <iogisdaki@gmail.com>
2024-02-28 17:10:49 +0200

    pong-9 final added sound effects

--- pong/main.lua
+++ pong/main.lua
@@ -29,6 +29,13 @@ function love.load()
 
     love.graphics.setFont(smallFont)
 
+    -- static cause its loaded in memory and kept in memory during execution
+    sounds = {
+            ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+            ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+            ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+        }
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -69,6 +76,7 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            sounds['paddle_hit']:play()
         end
 
         if ball:collides(player2) then
@@ -81,22 +89,26 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            sounds['paddle_hit']:play()
         end
 
         -- detect upper and lower screen boundary collision and reverse if collided
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4  -- -4 to account for the ball's size
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         -- if the ball reaches the left or right edge of the screen go to serve and update the score
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
+            sounds['score']:play()
 
             if player2Score == 10 then
                 winningPlayer = 2
@@ -109,6 +121,8 @@ function love.update(dt)
         elseif ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            sounds['score']:play()
+
             if player1Score == 10 then
                 winningPlayer = 1
                 gameState = 'done'
