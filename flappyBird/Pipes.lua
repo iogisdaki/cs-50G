69140dad5c29557da74924cff55b4359df19ede1
iogisdaki <iogisdaki@gmail.com>
2024-03-10 18:20:48 +0200

    made pipes be a pair

---
+++ flappyBird/Pipes.lua
@@ -0,0 +1,22 @@
+Pipes = Class{}
+
+local BOTTOM_PIPE_IMAGE = love.graphics.newImage('sprites/bottom_pipe.png')
+local TOP_PIPE_IMAGE = love.graphics.newImage('sprites/top_pipe.png') 
+local PIPE_SCROLL = -60
+
+function Pipes:init()
+    self.width = BOTTOM_PIPE_IMAGE:getWidth()
+    self.height = BOTTOM_PIPE_IMAGE:getHeight()
+    self.x = VIRTUAL_WIDTH
+    self.bottom_y = math.random(self.height, self.height * 1.39)
+    self.top_y = self.bottom_y - self.height - 100
+end
+
+function Pipes:update(dt)
+    self.x = self.x + PIPE_SCROLL * dt
+end
+
+function Pipes:render()
+    love.graphics.draw(BOTTOM_PIPE_IMAGE, self.x, self.bottom_y)
+    love.graphics.draw(TOP_PIPE_IMAGE, self.x, self.top_y)
+end
--- flappyBird/main.lua
+++ flappyBird/main.lua
@@ -1,6 +1,7 @@
 push = require 'push'
 Class = require "class"
 require 'Bird'
+require 'Pipes' 
 
 WINDOW_WIDTH = 2700
 WINDOW_HEIGHT = 1512
@@ -18,7 +19,12 @@ local GROUND_LOOPING_POINT = 200
 
 local bird = Bird()
 
+local pipes = {}
+local spawnTimer = 0
+local nextSpawnTime = 0
+
 function love.load()
+    math.randomseed(os.time())
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setTitle('Flappy Bird')
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -26,6 +32,8 @@ function love.load()
         fullscreen = false,
         resizable = false
     })
+
+    nextSpawnTime = math.random(2, 6)
 
     -- adding a key pressed table to keyboard
     love.keyboard.keyPressed = {}
@@ -57,6 +65,23 @@ function love.update(dt)
     -- modulus to loop back around
     groundScroll = (groundScroll + GROUND_SPEED * dt) % GROUND_LOOPING_POINT
     bird:update(dt)
+
+    spawnTimer = spawnTimer + dt
+    if spawnTimer > nextSpawnTime then
+        -- add new pipe objext to the table pipes
+        table.insert(pipes, Pipes())
+        spawnTimer = 0
+        nextSpawnTime = math.random(2, 3)
+    end
+
+    -- key-pipe pair in the table
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+        if pipe.x < -pipe.width then
+            table.remove(pipes, k)
+        end
+    end
+
     love.keyboard.keyPressed = {}
 end
 
@@ -64,6 +89,10 @@ function love.draw()
     push:start()
     -- goes backwards
     love.graphics.draw(background, 0, 0)
+    -- the order of drawing the pipes matters so that they look like they are sticking out of the ground
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
     bird:render()
     push:finish()
